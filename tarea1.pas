54853fbb21e7bf913b3b4214cacb2b641248b0ff
sebnun <sebnun@gmail.com>
2016-09-08 11:42:24 -0300

    more efficent algorithm

--- tarea1.pas
+++ tarea1.pas
@@ -18,7 +18,9 @@ begin
             numDivisores := 0;
             write(i, ':'); 
 
-            for j := 2 to (i - 1) do (* divisores triviales son 1 e i, no los uso *)
+            (* divisores triviales son 1 e i, no los uso
+            maximo divisor posible no trivial es i / 2 *)
+            for j := 2 to (i div 2) do 
             begin
 
                 if (i mod j) = 0 then (* j es divisor de i *)
@@ -34,7 +36,7 @@ begin
 
             end;
 
-            if numDivisores = 0 then (* no tiene divisores no triviales, es primo *)
+            if numDivisores = 0 then (* no tiene divisores no triviales *)
                 write(' es primo');
 
             writeLn;
